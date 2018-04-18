46503bf5e725b667cb941ebd4c51dfff55a2d04c
Your Name <you@example.com>
2018-04-18 12:00:40 +0100

    Criação do raodape e do topo

---
+++ Meu/src/app/Topo/Topo.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-topo.', /* essa declaração c eu posso criar a tag html*/
+    templateUrl: './Topo.component.html'   /* componente criado*/                 
+})
+export class TopoComponent {
+
+}
--- Meu/src/app/app.module.ts
+++ Meu/src/app/app.module.ts
@@ -1,13 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';/* decora, declara  o componente pra saber que será usado, na classe @NGmmodule*/
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './Topo/Topo.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    RodapeComponent /* nome do componente que sera utilizado*/
   ],
   imports: [
     BrowserModule
